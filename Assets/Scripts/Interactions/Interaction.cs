92d4410414372a6ff0e3ec0d998280c25563fe28
Haku <stacia1402@gmail.com>
2026-05-07 10:59:57 +0200

    Revert "Work on shelf level rn making books fall over work"
    This reverts commit 86235337e05021d25d511ae16cfa2af36620e3d9.

--- Assets/Scripts/Interactions/Interaction.cs
+++ Assets/Scripts/Interactions/Interaction.cs
@@ -11,8 +11,6 @@ public class Interaction : MonoBehaviour
     public GameObject stickyNote;
     public GameObject creditCard;
     public GameObject plant;
-    public GameObject bookCollider;
-    public GameObject[] books;
 
     void Awake()
     {
@@ -67,30 +65,6 @@ public class Interaction : MonoBehaviour
         {
             _gameManager.KnockOverPlant();
             plant.SetActive(false);
-        }
-
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("KnockOverBooks"))
-        {
-            if(_rat.dragging)
-            {
-                foreach (GameObject book in books)
-                {
-                    book.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-                }
-            }
-            else
-            {
-                foreach (GameObject book in books)
-                {
-                    book.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-                    book.GetComponent<Rigidbody>().useGravity = false;
-                }
-                
-            }
         }
     }
 
